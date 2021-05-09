d569921fdca7c0703f33042d7124ee377813baff
Nô Em <xuanson33bk@gmail.com>
2021-05-09 17:13:39 +0700

    feat: setup projects

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"]
+  extends: ["@commitlint/config-conventional"],
+ "type-enum" : [
+    'build',
+    'chore',
+    'ci',
+    'docs',
+    'feat',
+    'fix',
+    'perf',
+    'refactor',
+    'revert',
+    'style',
+    'test'
+  ];
 };
