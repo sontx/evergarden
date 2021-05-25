3a55615e3fcabb1f778eebc1a17a276606223076
Nô Em <xuanson33bk@gmail.com>
2021-05-25 12:55:32 +0700

    chore: auto calling refresh api to refresh token whenever the access token is expired

--- packages/webapp/src/utils/api.ts
+++ packages/webapp/src/utils/api.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
+import createAuthRefreshInterceptor from "axios-auth-refresh";
 
 const api = axios.create({});
+
+const refreshAuthLogic = (failedRequest: any) =>
+  axios.get("/api/auth/refresh").then((response) => {
+    return Promise.resolve();
+  });
+
+createAuthRefreshInterceptor(api, refreshAuthLogic, {
+  pauseInstanceWhileRefreshing: true
+});
 
 export default api;
