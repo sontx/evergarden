701858b36cc2f83f23c76c06f640cecfc792e6e2
Nô Em <xuanson33bk@gmail.com>
2021-06-01 13:08:37 +0700

    chore: add placeholder for searchbox

--- packages/webapp/src/features/search/SearchBox.tsx
+++ packages/webapp/src/features/search/SearchBox.tsx
@@ -77,6 +77,7 @@ export function SearchBox({
         >
           <InputGroup inside>
             <AutoComplete
+              placeholder="Search story..."
               onExit={handleHide}
               onEnter={handleShow}
               menuClassName="searchbox-menu"
