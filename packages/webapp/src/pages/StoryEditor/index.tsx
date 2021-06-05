5ba9e9e5b0c18a5709fa52898863f7fe286b5300
Nô Em <xuanson33bk@gmail.com>
2021-06-05 12:19:40 +0700

    chore: add view and back button on story editor page

--- packages/webapp/src/pages/StoryEditor/index.tsx
+++ packages/webapp/src/pages/StoryEditor/index.tsx
@@ -1,15 +1,18 @@
 import { StoryEditor } from "../../features/story-editor/StoryEditor";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { UserPage } from "../../components/UserPage";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import {
   fetchUserStoryAsync,
   selectStory,
 } from "../../features/story-editor/storyEditorSlice";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
+import { Button } from "rsuite";
+import { openStory } from "../../features/story/storySlice";
 
 export function StoryEditorPage() {
   const story = useAppSelector(selectStory);
+  const history = useHistory();
   const dispatch = useAppDispatch();
   const { url } = useParams<{ url: string }>();
 
@@ -19,10 +22,34 @@ export function StoryEditorPage() {
     }
   }, [dispatch, story, url]);
 
+  const handleBack = useCallback(() => {
+    history.push("/user/story");
+  }, [history]);
+
+  const handleView = useCallback(() => {
+    if (story) {
+      dispatch(openStory(history, story));
+    }
+  }, [dispatch, history, story]);
+
   const mode = !!url ? "update" : "create";
 
   return (
-    <UserPage title={mode === "update" ? "Update story" : "New story"}>
+    <UserPage
+      title={mode === "update" ? "Update story" : "New story"}
+      action={
+        <>
+          {mode === "update" && (
+            <Button onClick={handleView} appearance="link" size="sm">
+              View
+            </Button>
+          )}
+          <Button onClick={handleBack} appearance="link" size="sm">
+            Back
+          </Button>
+        </>
+      }
+    >
       <StoryEditor mode={mode} />
     </UserPage>
   );
