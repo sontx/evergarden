a725467bbf016b0596693a7f506f5801f8d4f940
sontx <xuanson33bk@gmail.com>
2021-09-05 00:08:28 +0700

    fix: follow button does not work

--- packages/webapp/src/features/story/withFollowSync.tsx
+++ packages/webapp/src/features/story/withFollowSync.tsx
@@ -1,38 +1,30 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback } from "react";
 import { useUnfollowStory } from "../following/hooks/useUnfollowStory";
 import { useFollowStory } from "../following/hooks/useFollowStory";
 import { withDebouncedClick } from "../../HOCs/withDebouncedClick";
+import { GetStoryDto } from "@evergarden/shared";
 
 export function withFollowSync(Component: React.ElementType) {
   const DebouncedComponent = withDebouncedClick(Component);
 
   return function (props: any) {
-    const story = props.story;
-    const { mutate: followStory } = useUnfollowStory();
-    const { mutate: unfollowStory } = useFollowStory();
-    const [isFollowing, setFollowing] = useState<boolean>();
-
-    useEffect(() => {
-      setFollowing(!!(story && story.history?.isFollowing));
-    }, [story]);
+    const story = props.story as GetStoryDto;
+    const { mutate: unfollowStory } = useUnfollowStory();
+    const { mutate: followStory } = useFollowStory();
 
     const handleFollow = useCallback(() => {
-      setFollowing((prevState) => {
-        const follow = !prevState;
-        if (follow) {
-          followStory(story.id);
-        } else {
-          unfollowStory(story.id);
-        }
-        return follow;
-      });
-    }, [followStory, story.id, unfollowStory]);
+      if (story.history?.isFollowing) {
+        unfollowStory(story.id);
+      } else {
+        followStory(story.id);
+      }
+    }, [followStory, story.history?.isFollowing, story.id, unfollowStory]);
 
     return (
       <DebouncedComponent
         {...props}
         onClick={handleFollow}
-        isFollowing={isFollowing}
+        isFollowing={!!story.history?.isFollowing}
       />
     );
   };
