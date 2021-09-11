794e90e2064ac229d1395078cbee292043521f73
Nhung Le <nhung.le@asnet.com.vn>
2021-09-12 00:09:07 +0700

    feat: add UserProfile's components

---
+++ packages/webapp/src/features/user/UserProfile/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import "./index.less"
+
+import { UserAvatar } from "./UserAvatar"
+import { UserName } from "./UserName"
+import {  SettingsPanel } from "../../settings/SettingsPanel"
+
+import { useUser } from '../hooks/useUser'
+
+export const UserProfile = () => {
+  const { data: user } = useUser()
+
+  return (
+    <>
+      <div className="profile">
+        <UserAvatar user={user}/>
+        <UserName user={user}/>
+      </div>
+      <SettingsPanel />
+    </>
+  );
+}
--- packages/webapp/src/features/user/hooks/useUpdateAvatar.ts
+++ packages/webapp/src/features/user/hooks/useUpdateAvatar.ts
@@ -2,7 +2,7 @@ import api from "../../../utils/api";
 import { GetUserDto } from "@evergarden/shared";
 import { useObjectMutation } from "../../../hooks/api-query/useObjectMutation";
 
-async function updateAvatar(file: File): Promise<GetUserDto> {
+async function updateAvatar(file: File | Blob): Promise<GetUserDto> {
   const formData = new FormData();
   formData.append("file", file);
   const response = await api.put("/api/users/avatar", formData, {
@@ -14,7 +14,7 @@ async function updateAvatar(file: File): Promise<GetUserDto> {
 }
 
 export function useUpdateAvatar() {
-  return useObjectMutation<File>("update-avatar", updateAvatar, {
+  return useObjectMutation<File | Blob>("update-avatar", updateAvatar, {
     relativeQueryKey: "user",
     updateQueryFrom: "request",
   });
