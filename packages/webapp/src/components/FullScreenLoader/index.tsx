840cba8f960d7187571fd0b6cc5af1afdca045fd
sontx <xuanson33bk@gmail.com>
2021-09-12 22:34:58 +0700

    fix: full screen loader is too hard to see

--- packages/webapp/src/components/FullScreenLoader/index.tsx
+++ packages/webapp/src/components/FullScreenLoader/index.tsx
@@ -2,14 +2,16 @@ import { CuteLoader } from "../CuteLoader";
 import { useIsDarkMode } from "../../features/global/hooks/useIsDarkMode";
 import { useOverlay } from "../../hooks/useOverlay";
 import { useNoBodyScrolling } from "../../hooks/useNoBodyScrolling";
+import { Backdrop } from "../Backdrop";
 
 export function FullScreenLoader() {
   useOverlay();
   useNoBodyScrolling();
   const { isDarkMode } = useIsDarkMode();
   return (
-    <div className="full-screen-loader rs-modal-backdrop backdrop backdrop-container">
+    <div className="full-screen-loader">
       <CuteLoader center dark={isDarkMode} />
+      <Backdrop noHeader/>
     </div>
   );
 }
