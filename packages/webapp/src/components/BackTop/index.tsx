d83938cce37aeb3195e05ba74d39d81be2225450
sontx <xuanson33bk@gmail.com>
2021-09-15 19:32:15 +0700

    chore: use ref to show/hide backtop

--- packages/webapp/src/components/BackTop/index.tsx
+++ packages/webapp/src/components/BackTop/index.tsx
@@ -1,6 +1,6 @@
 import { Fab } from "react-tiny-fab";
 import { Icon } from "rsuite";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useLayoutEffect, useRef } from "react";
 import { subscribeVerticalScrollDirection } from "../../utils/subscribe-vertical-scroll-direction";
 
 export function BackTop({
@@ -8,8 +8,8 @@ export function BackTop({
 }: {
   containerElement?: HTMLElement | null;
 }) {
-  const [show, setShow] = useState(false);
   const container = containerElement || document.documentElement;
+  const containerRef = useRef<HTMLElement>();
 
   const handleClick = useCallback(() => {
     container.scrollTo({
@@ -19,23 +19,30 @@ export function BackTop({
     });
   }, [container]);
 
+  useLayoutEffect(() => {
+    containerRef.current = document.querySelector(
+      "ul[data-testid='fab']",
+    ) as HTMLElement;
+    if (containerRef.current) {
+      containerRef.current.style.display = "none";
+    }
+  }, []);
+
   useEffect(() => {
     return subscribeVerticalScrollDirection(({ isDown }) => {
-      setShow(isDown);
+      if (containerRef.current) {
+        containerRef.current.style.display = isDown ? "unset" : "none";
+      }
     });
   }, [container]);
 
   return (
-    <>
-      {show && (
-        <Fab
-          onClick={handleClick}
-          event="click"
-          mainButtonStyles={{ width: "40px", height: "40px", opacity: 0.5 }}
-          style={{ bottom: "40px", right: "10px", margin: 0 }}
-          icon={<Icon icon="up" />}
-        />
-      )}
-    </>
+    <Fab
+      onClick={handleClick}
+      event="click"
+      mainButtonStyles={{ width: "40px", height: "40px", opacity: 0.5 }}
+      style={{ bottom: "40px", right: "10px", margin: 0 }}
+      icon={<Icon icon="up" />}
+    />
   );
 }
